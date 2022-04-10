66d91fa8485b0d4bc7bacf7a9ddc65d06a9adad3
Luís Felipe Zanatto <luisfelipe.zanatto@gmail.com>
2022-04-10 19:22:29 -0300

    Add OpenAPI doc Works at: http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/felzan/coffeeshop/api/controller/StoreController.java
+++ src/main/java/com/felzan/coffeeshop/api/controller/StoreController.java
@@ -3,11 +3,14 @@ package com.felzan.coffeeshop.api.controller;
 import com.felzan.coffeeshop.api.repository.StoreDocument;
 import com.felzan.coffeeshop.api.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,7 @@ public class StoreController {
 
     private final StoreService storeService;
 
-    @PostMapping
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<StoreDocument> postStore(@RequestBody StoreRequest request) {
         return ResponseEntity.ok().body(storeService.createStore(request));
     }
