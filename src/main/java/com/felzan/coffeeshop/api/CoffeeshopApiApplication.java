a2e50209d33c57f60e769a76b83391728ca917c9
Luís Felipe Zanatto <luisfelipe.zanatto@gmail.com>
2022-04-10 15:31:04 -0300

    Start Spring

---
+++ src/main/java/com/felzan/coffeeshop/api/CoffeeshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.felzan.coffeeshop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felzan/coffeeshop/api/CoffeeshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felzan.coffeeshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
